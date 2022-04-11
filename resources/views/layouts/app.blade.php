f06b52c840e12ece032a555b3bf45ba2dca78642
IgBuS <i.b@msulab.ru>
2022-04-11 13:09:35 +0300

    step 10

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}"></script>
     </head>
+    @include('menus.menu')
     <body>
         <div class="container mt-4">
             <h1>@yield('header')</h1>
